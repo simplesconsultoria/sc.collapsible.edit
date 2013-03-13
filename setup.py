b56ad93b2bba1aefae2d3790264d6b31256afc3c
hvelarde <hector.velarde@gmail.com>
2013-03-13 18:34:30 -0300

    update package documentation

--- setup.py
+++ setup.py
@@ -1,38 +1,42 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
 version = '0.1'
-
-long_description = (
-    open('README.txt').read()
-    + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
-    + '\n')
+long_description = open("README.txt").read() + "\n" + \
+    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+    open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='sc.collapsible.edit',
       version=version,
       description="A collapsible edit form for Plone content types.",
       long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='gpl',
+          "Development Status :: 3 - Alpha",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+#          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+#          "Framework :: Plone :: 4.3",
+          "Intended Audience :: End Users/Desktop",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: JavaScript",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='plone editing collapsible',
+      author='Juan Pablo Giménez',
+      author_email='jpg@simplesconsultoria.com.br',
+      url='https://github.com/simplesconsultoria/sc.collapsible.edit',
+      license='GPLv2',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['sc', 'sc.collapsible'],
@@ -45,7 +49,7 @@ setup(name='sc.collapsible.edit',
       extras_require={'test': ['plone.app.testing']},
       entry_points="""
       # -*- Entry points: -*-
-  	  [z3c.autoinclude.plugin]
-  	  target = plone
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
